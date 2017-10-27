4ec0fd7b16e0fa3e4bd1d1711f379aca0abce32f
YangHangbin <yanghangbin@gmail.com>
2017-10-27 13:22:26 +0800

    update passport

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,21 +1,28 @@
 'use strict'
 
-const jwt = require('jsonwebtoken')
 const passport = require('koa-passport')
 const LocalStrategy = require('passport-local').Strategy
 const WeiboStrategy = require('passport-weibo').Strategy
+const WechatStrategy = require('passport-wechat').Strategy
 
 /** 
  * 将user信息序列化至session/cookie 
  * 可在此处生成jsonwebtoken
  */
 passport.serializeUser((user, done) => {
-
+  const jwt = require('jsonwebtoken')
+  const token = jwt.sign(user._id, 'screct_token')
+  done(null, token)
 })
 
 /** 根据session中的token反序列化 */
-passport.deserializeUser((token, done) => {
-
+passport.deserializeUser( async (token, done) => {
+  const jwt = require('jsonwebtoken')
+  const _id = jwt.verify(token, 'secret_token')
+  /**
+   * const user = await User.findOne({_id: _id})
+   * done(null, user)
+   */
 })
 
 /** 通过用户名密码验证登录 */
@@ -39,13 +46,42 @@ passport.use(new WeiboStrategy({
    * return done(null, user)
    */
   console.log(profile)
+  return done(null, user)
+}))
+
+/** 通过微信登录 */
+passport.use(new WechatStrategy({
+  appID: "{APPID}",
+  name: "{默认为wechat,可以设置组件的名字}",
+  appSecret: "{APPSECRET}",
+  client: "{wechat|web}",
+  callbackURL: "{CALLBACKURL}",
+  scope: "{snsapi_userinfo|snsapi_base}",
+  state: "{STATE}",
+  getToken: "{getToken}",
+  saveToken: "{saveToken}"
+}, (accessToken, refreshToken, profile, expires_in, done) => {
+  /**
+   * const user = await User.findOrCreate({wechatId: profile.id})
+   * return done(null, user)
+   */
+  return done(null, user)
 }))
 
 module.exports = router => {
 
+  /** 注册微博回调 */
   router.get('/auth/weibo', passport.authenticate('weibo'))
   router.get('/auth/weibo/callback', passport.authenticate('weibo', (param1, param2) => {
-    console.log(param1, param2)
+    console.log(param1)
+    console.log(param2)
+  }))
+
+  /** 注册微信回调 */
+  router.get('/auth/wechat', passport.authenticate('wechat'))
+  router.get('/auth/wechat/callback', passport.authenticate('wechat', (param1, param2) => {
+    console.log(param1)
+    console.log(param2)
   }))
 
   return passport
